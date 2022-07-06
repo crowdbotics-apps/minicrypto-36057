7505584e8dfb7ad142caffb72e96c68813d8e312
Crowdbotics <team@crowdbotics.com>
2022-07-06 10:14:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "minicrypto_36057",
+  url: "https://minicrypto_36057.botics.co",
+  api: "https://minicrypto_36057.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
